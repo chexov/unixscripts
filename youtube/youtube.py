d7e9dd1db759b04b116b243e63f2b7e8eef8125b
chexov <chexov@unixmaniacs.com>
2009-10-02 18:11:48 -0400

    now youtube.py can download youtube playlists

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -1,33 +1,92 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import optparse
 import logging
 import urllib2
-import os
+import urllib
 import sys
+import os
 import re
-from xml.dom import minidom
+from HTMLParser import HTMLParser
 
 logging.basicConfig()
-log = logging.getLogger("youtube.downloader")
-log.setLevel(logging.DEBUG)
+LOG = logging.getLogger("youtube.downloader")
+LOG.setLevel(logging.DEBUG)
 
-def downloadFileByUrl(url):
+def _reporthook(numblocks, blocksize, filesize, url=None):
+    #print "reporthook(%s, %s, %s)" % (numblocks, blocksize, filesize)
+    base = os.path.basename(url)
+    #base = "Progress "
+    #XXX Should handle possible filesize=-1.
+    try:
+        percent = min((numblocks*blocksize*100)/filesize, 100)
+    except:
+        percent = 100
+    if numblocks != 0:
+        sys.stdout.write("\b"*80)
+        sys.stdout.write("%-66s %3d%%" % (base, percent))
+
+
+def geturl(url, dst):
+    LOG.info("Downloading url '%s' to '%s'" % (url, dst) )
+    if sys.stdout.isatty():
+        return urllib.urlretrieve(url, dst, lambda nb, bs, fs, url=url: _reporthook(nb,bs,fs,dst))
+        #sys.stdout.write('\n')
+    else:
+        return urllib.urlretrieve(url, dst)
+
+def downloadFileByUrl(url, filename=None):
    data = None
    try:
        f = urllib2.urlopen(url)
-       data = f.read()
-       f.close()
+       if filename:
+           return geturl(url, filename)
+       else:
+           data = f.read()
+           f.close()
    except:
-       log.error ("While downloading file %s: %s" %(url, sys.exc_value) )
+       LOG.error ("While downloading file %s: %s" %(url, sys.exc_value) )
    finally:
        return data
 
 
-def dataToFile(filename, data):
-   f = open(filename,'wb')
-   f.write(data)
-   f.close()
+
+class YoutubePlaylistHTMLParser(HTMLParser):
+    """HTMLParser class which extract youtube video IDs
+       from HTML page
+    """
+    PLAYLIST_ITEMS = set()
+    
+    def __extract_video_id_from_uri(self, uri):
+        """
+        GET uri like '/watch?v=AsXf9v&param=1&p=3#junk'
+        RETURNS value for 'v' parameter --> 'AsXf9v'
+        """
+        uri = uri.replace('&', ';')
+        uri = uri.replace('?', ';')
+        req, params = urllib.splitattr(uri)
+        for item in params:
+            k, v = urllib.splitvalue(params[0])
+            if k == 'v':
+                return v
+        raise ValueError("Can't find parameter 'v' from '%s'" % uri)
+    
+    def handle_starttag(self, tag, attrs):
+        if not tag == 'a':
+            return 1
+        
+        # Building dict() from attrs list(). It's easy to dealing with dict() later...
+        _attrs_dict = {}
+        for attr in attrs:
+            key, value = attr
+            _attrs_dict[key] = value
+        
+        if _attrs_dict.get('id') and _attrs_dict.get('id').find('video-long-title') > -1:
+            # We need only HREFs with 'id' == 'video-long-title.*'
+            vid = self.__extract_video_id_from_uri(_attrs_dict['href'])
+            self.PLAYLIST_ITEMS.add(vid)
+            LOG.info("Found video id %s for %s" % (vid, _attrs_dict.get('title')) )
 
 
 class Youtube(object):
@@ -36,10 +95,12 @@ class Youtube(object):
     @staticmethod
     def retriveYoutubePageToken(ID, htmlpage=None):
         """
-        l="var fullscreenUrl = '/watch_fullscreen?fs=1&fexp=900142%2C900030%2C900162&iv_storage_server=http%3A%2F%2Fwww.google.com%2Freviews%2Fy%2F&creator=amiablewalker&sourceid=r&video_id=VJyTA4VlZus&l=353&sk=QtBR18Y95jsDyLXHgv9jbMu0ghb3MxoSU&fmt_map=34%2F0%2F9%2F0%2F115%2C5%2F0%2F7%2F0%2F0&t=vjVQa1PpcFPt0HhU0HkTG6A75-QxhAiV6WuMqB2a4r4%3D&hl=en&plid=AARnlkLz-d6cbsVe&vq=None&iv_module=http%3A%2F%2Fs.ytimg.com%2Fyt%2Fswf%2Fiv_module-vfl89178.swf&cr=US&sdetail=p%253Afriendfeed.com%2Feril&title=How To Learn Any Accent Part 1';"
+        Magick method witch extract session token from 'htmlpage'.
+        Session token needed for video download URL
         """
         if not htmlpage:
             url = "http://www.youtube.com/watch?v=%s" % ID
+            #htmlpage="var fullscreenUrl = '/watch_fullscreen?fs=1&fexp=900142%2C900030%2C900162&iv_storage_server=http%3A%2F%2Fwww.google.com%2Freviews%2Fy%2F&creator=amiablewalker&sourceid=r&video_id=VJyTA4VlZus&l=353&sk=QtBR18Y95jsDyLXHgv9jbMu0ghb3MxoSU&fmt_map=34%2F0%2F9%2F0%2F115%2C5%2F0%2F7%2F0%2F0&t=vjVQa1PpcFPt0HhU0HkTG6A75-QxhAiV6WuMqB2a4r4%3D&hl=en&plid=AARnlkLz-d6cbsVe&vq=None&iv_module=http%3A%2F%2Fs.ytimg.com%2Fyt%2Fswf%2Fiv_module-vfl89178.swf&cr=US&sdetail=p%253Afriendfeed.com%2Feril&title=How To Learn Any Accent Part 1';"
             htmlpage = urllib2.urlopen(url).read()
         match = re.search(r', "t": "([^&"]+)"', htmlpage)
         if match:
@@ -62,34 +123,6 @@ class Youtube(object):
         return title
     
     @staticmethod
-    def retriveYoutubeTokenUsingAPI(ID):
-        """Getting youtube token.
-         token is need for building FLV URL"""
-        youtube_token = None
-        url = "http://www.youtube.com/api2_rest?method=youtube.videos.get_video_token&video_id=%s" % ID
-        u = None
-        try:
-            log.debug("urlopen %s" % url)
-            u = urllib2.urlopen(url)
-        except:
-            log.error("Can't retrive token using %s: %s" % (url, sys.exc_value))
-
-        if u:
-            # getting youtube token from XML
-            rawxml = u.read()
-            try:
-                #log.debug("youtube XML answer: %s" % rawxml)
-                xmldoc = minidom.parseString(rawxml)
-                grammarNode = xmldoc.firstChild
-                refNode = grammarNode.childNodes[0]
-                pNode = refNode.childNodes[0]
-                youtube_token = pNode.data
-                log.debug("got youtube token: %s" % youtube_token)
-            except:
-                log.error("Can't exctract youtube token from XML '%s': %s" % (rawxml, sys.exc_value) )
-        return youtube_token
-    
-    @staticmethod
     def getHDVideourlByID(ID):
         videourl = None
         token = Youtube.retriveYoutubePageToken(ID)
@@ -106,64 +139,90 @@ class Youtube(object):
         return videourl
         
     @staticmethod
-    def run(serviceID, outFilePath=None):
-        url = "http://www.youtube.com/watch?v=%s" % serviceID
+    def run(youtube_id, outFilePath=None):
+        """
+        GET youtube video ID 'youtube_id'
+        RETURNS True is file properly downloaded and saved to local disk
+                False in case of error
+        """
+        url = "http://www.youtube.com/watch?v=%s" % youtube_id
         htmlpage = None
         if not outFilePath:
             htmlpage = urllib2.urlopen(url).read()
-            title = Youtube.retriveYoutubePageTitle(serviceID, htmlpage, clean=True)
+            title = Youtube.retriveYoutubePageTitle(youtube_id, htmlpage, clean=True)
             outFolder = os.getcwd()
             outFilePath = os.path.join(os.getcwd(), title + '.mp4')
         
+        outFilePath_tmp = outFilePath + ".part"
         data = None
         finished = False
         
         # if file exist on local node, do not download FLV one more.
         if os.path.isfile(outFilePath):
-            log.warning("We already have %s. Not retrieving" % (outFilePath))
+            LOG.warning("We already have %s. Not retrieving" % (outFilePath))
             finished = True
             return finished
-
-        log.debug("Trying to get HD video")
-        url = Youtube.getHDVideourlByID(serviceID)
-        if not url:
-            log.debug("Can't get HD video url")
-        else:
-            log.debug("Downloading %s -> %s" % (url, outFilePath))
-            data = downloadFileByUrl(url)
-            if data:
-                log.info("saving %s into %s" % (serviceID, outFilePath))
-                dataToFile(outFilePath, data)
-                finished = True
-                return finished
-            else:
-                log.debug("no data while downloading '%s'" % url)
         
-        log.debug("Trying to get HQ video")
-        url = Youtube.getHQVideourlByID(serviceID)
+        LOG.debug("Trying to get HD video")
+        url = Youtube.getHDVideourlByID(youtube_id)
         if not url:
-            log.debug("Can't get HQ video url")
+            LOG.debug("Can't get HD video url")
         else:
-            log.debug("Downloading %s -> %s" % (url, outFilePath))
-            data = downloadFileByUrl(url)
-            if data:
-                log.info("saving %s into %s" % (serviceID, outFilePath))
-                dataToFile(outFilePath, data)
-                finished = True
-                return finished
-            else:
-                log.debug("no data while downloading '%s'" % url)
+            #LOG.debug("Downloading %s -> %s" % (url, outFilePath))
+            if not downloadFileByUrl(url, outFilePath_tmp):
+                LOG.debug("no data while downloading '%s'" % url)
+        
+        LOG.debug("Trying to get HQ video")
+        url = Youtube.getHQVideourlByID(youtube_id)
+        if not url:
+            LOG.debug("Can't get HQ video url")
+        else:
+            #LOG.debug("Downloading %s -> %s" % (url, outFilePath))
+            if not downloadFileByUrl(url, outFilePath_tmp):
+                LOG.debug("no data while downloading '%s'" % url)
+        os.rename(outFilePath_tmp, outFilePath)
         return finished
+    
+    
+    @staticmethod
+    def get_playlist_video_ids(playlist_id, html=None):
+        """
+        GET playlist_id
+        RETURNS set() of all video ids from that playlist
+        
+        Explanation:
+          for the URL http://www.youtube.com/view_play_list?p=8EE54070B382E73A
+          'playlist_id' shold be '8EE54070B382E73A'
+        """
+        result = set()
+        playlist_url = "http://www.youtube.com/view_play_list?p=%s" % playlist_id
+        
+        if not html:
+            LOG.info('Downloading playlist %s from "%s"' % (playlist_id, playlist_url))
+            html = urllib2.urlopen(playlist_url).read()
+            #html = open('youtube.playlist.example.html').read()
+        ypp = YoutubePlaylistHTMLParser()
+        ypp.feed(html)
+        return ypp.PLAYLIST_ITEMS
+
 
 
 if __name__ == "__main__":
-    log.setLevel(logging.DEBUG)
-    if len(sys.argv) == 2:
-        ID = sys.argv[1]
-        Youtube.run(ID)
-    elif len(sys.argv) == 3:
-        Youtube.run(serviceID=sys.argv[1], outFilePath=sys.argv[2])
+    LOG.setLevel(logging.DEBUG)
+    usage = "usage: %prog <youtube-video-id> [youtube-video-id,..]\n       %prog -p <playlist id>"
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option("-p", "--playlist", dest="playlist",
+            help="Download all playlist videos to the current directory", default=None)
+    
+    (options, args) = parser.parse_args()
+    
+    if options.playlist:
+        for vID in Youtube.get_playlist_video_ids('8EE54070B382E73A'):
+            Youtube.run(vID)
+    elif len(args) > 0:
+        for vID in args:
+            Youtube.run(vID)
     else:
-        print "%s <Youtube ID> [<Out Filename>]" % sys.argv[0]
-        sys.exit(13)
+        parser.print_help()
+    
 

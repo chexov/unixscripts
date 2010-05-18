307f9586959d22247fdbea3b0386e0d8c71b9a94
Anton P. Linevich <anton@linevich.com>
2010-05-18 06:22:58 +0300

    working on universality a little bit

--- aws-cli/update-tools.sh
+++ aws-cli/update-tools.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 set -xue
-s3cmd ls s3://ec2-downloads > ec2-downloads.list
 cd `dirname $0`
+
+# Hack?. Getting list of amazon's S3 ec2-downloads bucket
+s3cmd ls s3://ec2-downloads > ec2-downloads.list
+
 TOOLS="AutoScaling CloudWatch ec2-ami-tools ec2-api-tools ElasticLoadBalancing"
 for tool in $TOOLS; do
+    # Get only first (freshest) zip file
     key=$(egrep 'zip$' ec2-downloads.list | sort -nr | grep $tool | head -1 | awk '{ print $4}')
-    s3cmd get $key --skip-existing
+
     b=$(basename $key)
-    unzip -u $b
+    url="http://ec2-downloads.s3.amazonaws.com/$b"
+    wget -c $url
 done
-cd $OLDPWD
+
+cd -
 
